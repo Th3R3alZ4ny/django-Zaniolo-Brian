e325d44f1cff9bdb757449fd11901f46b746a48d
Th3R3alZ4ny <bzaniolo83@gmail.com>
2021-11-04 12:50:43 +0100

    livello 2

--- prima_app/urls.py
+++ prima_app/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from prima_app.views import homepage
+from prima_app.views import welcome
+from prima_app.views import lista
 
 app_name="prima_app"
 urlpatterns=[
     path('',homepage,name="homepage"),
+    path('welcome',welcome,name="welcome"),
+    path('lista',lista,name="lista"),
 ]
