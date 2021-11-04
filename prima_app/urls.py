a32f07f70ad154d521d1902dc135d0390fbe0391
Th3R3alZ4ny <bzaniolo83@gmail.com>
2021-11-04 12:59:24 +0100

    livello 4

--- prima_app/urls.py
+++ prima_app/urls.py
@@ -3,11 +3,13 @@ from prima_app.views import homepage
 from prima_app.views import welcome
 from prima_app.views import lista
 from prima_app.views import chi_siamo
+from prima_app.views import variabili
 
 app_name="prima_app"
 urlpatterns=[
     path('',homepage,name="homepage"),
     path('welcome',welcome,name="welcome"),
     path('lista',lista,name="lista"),
-    path('chi_siamo',chi_siamo,name="chi_siamo")
+    path('chi_siamo',chi_siamo,name="chi_siamo"),
+    path('variabili',variabili,name="variabili")
 ]
