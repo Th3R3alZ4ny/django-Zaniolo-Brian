6c8ab702737d00a5100da53c675b7668f46f2167
Th3R3alZ4any <bzaniolo83@gmail.com>
2022-01-18 22:12:27 +0100

    prova pratica 1

--- prova_pratica_1/views.py
+++ prova_pratica_1/views.py
@@ -1,15 +1,19 @@
+from multiprocessing import context
 from django.shortcuts import render
-from .models import Materie
 # Create your views here.
 def view_b(request):
-    materie=Materie.objects.all()
-    context={"materie":materie}
-    print(context)
-    return render(request,"view_b.html",context)
-def view_c(request):
-    voti = {
-        'Giuseppe Gullo':[("Matematica",9,0),("Italiano",7,3),("Inglese",7.5,4),("Storia",7.5,4),("Geografia",5,7)],
-        'Antonio Barbera':[("Matematica",8,1),("Italiano",6,1),("Inglese",9.5,0),("Storia",8,2),("Geografia",8,1)],
-        'Nicola Spina':[("Matematica",7.5,2),("Italiano",6,2),("Inglese",4,3),("Storia",8.5,2),("Geografia",8,2)]
+    materie={
+        'm1':"Matematica",
+        'm2':"Italiano",
+        'm3':"Inglese",
+        'm4':"Storia",
+        'm5':"Geografia"
     }
-    return render(request,"view_c.html",voti)
+    return render(request,"view_b.html",materie)
+def view_c(request):
+    context = {
+        'GiuseppeGullo':[["Matematica",9,0],("Italiano",7,3),("Inglese",7.5,4),("Storia",7.5,4),("Geografia",5,7)],
+        'AntonioBarbera':[("Matematica",8,1),("Italiano",6,1),("Inglese",9.5,0),("Storia",8,2),("Geografia",8,1)],
+        'NicolaSpina':[("Matematica",7.5,2),("Italiano",6,2),("Inglese",4,3),("Storia",8.5,2),("Geografia",8,2)]
+    }
+    return render(request,"view_c.html",context)
